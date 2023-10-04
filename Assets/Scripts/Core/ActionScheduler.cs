ff589f26895f070da0862ba102296877f02e2bd4
agarasia <aumgarasia@gmail.com>
2023-10-03 21:16:52 -0700

    First Commit for second Project

---
+++ Assets/Scripts/Core/ActionScheduler.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+/*
+    Why Need a Scheduler script?
+
+    Circular dependencies are very dangerous in runtime.
+    Say Movement and Combat dependencies have a mutual dependency 
+    on each other. If any of the above has a bug, then both the 
+    namespaces will face the issue!
+
+*/
+
+namespace RPG.Core
+{
+    public class ActionScheduler : MonoBehaviour 
+    {
+        IAction currentAction;
+        public void StartAction(IAction action)
+        {
+            // We need to be able to cancel an action at the right time
+            if(currentAction==action)   return;
+            if(currentAction!=null)
+            {
+                currentAction.Cancel();
+            }
+            currentAction = action;
+        }
+
+        public void CancelCurrentAction()
+        {
+            StartAction(null);
+        }
+    }
+}
